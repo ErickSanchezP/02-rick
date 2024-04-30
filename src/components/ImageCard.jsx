12400040ecda023bba1c1f62ba4067bee779f724
Erick <erick.sanchez,ponce@gmail.com>
2024-04-29 23:37:44 -0600

    names included

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -2,6 +2,7 @@ const ImageCard = ({ url, name }) => {
   return (
     <div className='image-container'>
       <img src={url} alt={name} className='image-view' />
+      <h2>{name}</h2>
     </div>
   )
 }
